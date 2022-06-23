0bdb9caea32e4c692ffbbbf4120802cac68aa997
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2022-06-22 22:13:02 -0400

    Add icons to Nav component

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
+// import {server} from '../config'
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+
 import Typography from '@mui/material/Typography';
 import { Container, Grid } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material';
-// import Brightness6Icon from '@mui/icons-material/Brightness6';
-// import { ColorLensIcon } from '@mui/icons-material';
+// import Button from '@mui/material';
+import Brightness6Icon from '@mui/icons-material/Brightness6';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';// import { ColorLensIcon } from '@mui/icons-material';
 // import ColorLensIcon from '@mui/icons-material/ColorLens';
+
 
 const Nav = () => {
     return (
@@ -18,17 +23,28 @@ const Nav = () => {
         justifyContent="space-between"
         alignItems="center">
             <Grid item>
-                <Typography variant='h6'>
-                    Quran Journey
-                </Typography>
+
+              <Grid container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center">
+                <Grid item>
+                  <IconButton color="dark">
+                      <DarkModeRoundedIcon />
+                  </IconButton>  
+                </Grid>
+                <Grid item>
+                  <Typography variant='h6'>
+                      Quran Journey
+                  </Typography>
+                </Grid>
+              </Grid>
             </Grid>
+
             <Grid item>
-                {/* <IconButton>
-                    <ColorLensIcon />
-                </IconButton>  Error: Element type is invalid: expected a string*/}
-                <Typography variant='h6'>
-                    O
-                </Typography>
+                <IconButton color="dark">
+                    <Brightness6Icon />
+                </IconButton>  
             </Grid>
         </Grid>
       </Container>
