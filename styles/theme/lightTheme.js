c6d1909909b4b19542c3ed40b0bb2df9f0ac64e5
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2022-06-22 22:11:54 -0400

    Add arabic typography and additional colors to theme

--- styles/theme/lightTheme.js
+++ styles/theme/lightTheme.js
@@ -15,6 +15,16 @@ const lightTheme = createTheme({
       light: '#d5e86a',
       dark: '#aace12',
     },
+    dark: {
+      main: "#000000",
+      light: '#d5e86a',
+      dark: '#aace12',
+    },
+    sage: {
+      main: "#CCECE9",
+      light: "#F5F4F3",
+      dark: "#bee520"
+    }
   },
   typography: {
     h1: {
@@ -22,19 +32,54 @@ const lightTheme = createTheme({
       fontWeight: 600,
       letterSpacing: '-0.02em',
       fontSize: '5rem',
+      lineHeight: 1.2,
+    },
+    h1_ar: {
+      fontFamily: '"IBM Plex Sans Arabic", "Helvetica", "Arial", sans-serif',
+      fontWeight: 500,
+      fontSize: '3rem',
+      lineHeight: 1.5,
+
+
     },
     h2: {
       fontWeight: 600,
       fontFamily: '"Poppins", "Helvetica", "Arial", sans-serif',
+      lineHeight: 1.5,
+
+    },
+    h2_ar: {
+      fontFamily: '"IBM Plex Sans Arabic", "Helvetica", "Arial", sans-serif',
+      fontWeight: 500,
+      fontSize: '2.5rem',
+      lineHeight: 1.5,
+
     },
     h3: {
       fontWeight: 600,
       letterSpacing: '-0.01em',
       fontFamily: '"Poppins", "Helvetica", "Arial", sans-serif',
+      lineHeight: 1.5,
+
+    },
+    h3_ar: {
+      fontFamily: '"IBM Plex Sans Arabic", "Helvetica", "Arial", sans-serif',
+      fontWeight: 500,
+      fontSize: '2rem',
+      lineHeight: 1.5,
+
     },
     h4: {
       fontWeight: 600,
       fontFamily: '"Poppins", "Helvetica", "Arial", sans-serif',
+      lineHeight: 1.5,
+    },
+    h4_ar: {
+      fontFamily: '"IBM Plex Sans Arabic", "Helvetica", "Arial", sans-serif',
+      fontWeight: 500,
+      fontSize: '1.5rem',
+      lineHeight: 1.5,      
+
     },
     h5: {
       fontFamily: '"Poppins", "Helvetica", "Arial", sans-serif',
@@ -52,17 +97,21 @@ const lightTheme = createTheme({
     },
     body1: {
       fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
-      fontWeight: 500,
+      fontWeight: 600,
       lineHeight: 1.7,
       fontSize: '1.1rem',
     },
     body2: {
       fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
+      fontWeight: 500,
+      lineHeight: 1.7,
+      fontSize: '1rem',
     },
     button: {
-      fontWeight: 600,
+      fontWeight: 700,
       fontFamily: '"Poppins", "Helvetica", "Arial", sans-serif',
       textTransform: 'none',
+      letterSpacing: '-0.01em',
     },
   },
   components: {
@@ -83,6 +132,38 @@ const lightTheme = createTheme({
         },
       },
     },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '50px',
+          paddingLeft: '1.3em',
+          paddingRight: '1.3em',
+          paddingTop: '0.75em',
+          paddingBottom: '0.75em',
+        }
+      },
+      variants: [
+        {
+          // Come back to this
+          props: { variant: 'main' },
+          style: {
+            borderColor: '#000000',
+            color: '#000000'
+          }
+        }
+      ]
+    },
+    MuiAccordion: {
+      // Work in progress
+      styleOverrides: {
+        root: {
+          borderRadius: '32px',
+          border: 'none',
+          boxShadow: 'none',
+          backgroundColor: '#EEF1EC'
+        }
+      }
+    }
   },
 })
 
