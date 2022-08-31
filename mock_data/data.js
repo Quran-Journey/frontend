e0f95cedd88671be4c6c1ab53b5f8925ff71392d
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2022-08-31 00:42:46 -0400

    Add and fix data

--- mock_data/data.js
+++ mock_data/data.js
@@ -1,41 +1,41 @@
 export const lessons = [
     {
-        id: '97',
+        id: 97,
         name: "Al-Qadr",
         name_ar: 'القَدر',
         meaning: 'The Power',
         verses: '5'
     },
     {
-        id: '98',
+        id: 98,
         name: 'Al-Bayyina',
         name_ar: 'البَیّنَة',
         meaning: 'The Clear Proof',
         verses: '8'
     },
     {
-        id: '99',
+        id: 99,
         name: 'Al-Zalzalah',
         name_ar: 'الزّلزَلة',
         meaning: 'The Earthquake',
         verses: '8'
     },
     {
-        id: '100',
+        id: 100,
         name: "Al 'adiyat",
         name_ar: 'العَادیَات',
         meaning: 'The Courser',
         verses: '11'
     },
     {
-        id: '101',
+        id: 101,
         name: 'Al-Qaria',
         name_ar: 'القَارعَة',
         meaning: 'The Calamity',
         verses: '11'
     },
     {
-        id: '102',
+        id: 102,
         name: 'At-Takathur',
         name_ar: 'التّکاثُر',
         meaning: 'Rivalry in World Increase',
