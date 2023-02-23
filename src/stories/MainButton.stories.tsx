4c777d850c9439db8d4b05077fa2030cd6ef7e9a
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2023-02-23 12:36:27 -0500

    Add delete and add buttons to storybook

--- src/stories/MainButton.stories.tsx
+++ src/stories/MainButton.stories.tsx
@@ -3,6 +3,9 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { MainButton } from './MainButton';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import ClearIcon from '@mui/icons-material/Clear';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 export default {
   title: 'UI Elements/Button',
@@ -19,7 +22,6 @@ JumpToLesson.args = {
   color: 'periwinkle',
   size: 'small',
   label: 'Jump to Lesson',
-  iconEnd: <ArrowCircleRightOutlinedIcon />
 };
 
 export const Back = Template.bind({});
@@ -37,4 +39,22 @@ Next.args = {
   size: 'small',
   label: 'Next',
   iconEnd: <ArrowCircleRightOutlinedIcon />
+};
+
+export const Delete = Template.bind({});
+Delete.args = {
+  color: 'error',
+  size: 'small',
+  label: 'Delete',
+  variant: 'outlined',
+  iconEnd: <HighlightOffIcon />
+};
+
+export const Add = Template.bind({});
+Add.args = {
+  color: 'black',
+  size: 'small',
+  label: 'Add',
+  variant: 'outlined',
+  iconEnd: <AddCircleOutlineIcon />
 };
