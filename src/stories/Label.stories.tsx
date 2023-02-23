ec13097d55772849e2350f71729940ee24a22ecc
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2023-02-23 14:55:09 -0500

    Add label component stories to storybook

---
+++ src/stories/Label.stories.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Label } from './Label';
+
+export default {
+  title: 'UI Basic Elements/Label',
+  component: Label,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Label>;
+
+const Template: ComponentStory<typeof Label> = (args) => <Label {...args} />;
+
+export const MainLabel = Template.bind({});
+MainLabel.args = {
+  color: 'lavendar',
+  label: 'Label',
+};
+
+export const NumberOfLessonsLabel = Template.bind({});
+NumberOfLessonsLabel.args = {
+  color: 'periwinkle',
+  label: '3 Lessons',
+};
+
+export const SurahNumberLabel = Template.bind({});
+SurahNumberLabel.args = {
+  color: 'lavendar',
+  label: '01',
+  fontWeight: '600',
+  size: 'small'
+};
+
+export const LessonLabel = Template.bind({});
+LessonLabel.args = {
+  color: 'periwinkle',
+  label: 'Lesson 1',
+  fontWeight: '600',
+};
+
+export const Periwinkle = Template.bind({});
+Periwinkle.args = {
+  color: 'periwinkle',
+  label: 'Linguistic',
+  size: 'small'
+};
+
+export const BabyBlue = Template.bind({});
+BabyBlue.args = {
+  color: 'babyBlue',
+  label: 'Tafsir',
+  size: 'small'
+};
+
+export const Mint = Template.bind({});
+Mint.args = {
+  color: 'mint',
+  label: 'Comments',
+  size: 'small'
+};
