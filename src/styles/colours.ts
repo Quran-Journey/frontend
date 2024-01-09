901cd5aceeee8e7b6bcb2381cb1a192fe2a511f4
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2024-01-09 16:38:44 -0500

    Create colour const

---
+++ src/styles/colours.ts
@@ -0,0 +1,23 @@
+interface Colours {
+  darkGreen: string;
+  forestGreen: string;
+  emrald: string;
+  lightGreen: string;
+  white: string;
+  black: string;
+  darkGrey: string;
+  grey: string;
+  lightGrey: string;
+}
+
+export const colours: Colours = {
+  darkGreen: '#003C2D',
+  forestGreen: '#005B45',
+  emrald: '#00916E',
+  lightGreen: '#CCF9CE',
+  white: '#FFFFFF',
+  black: '#000000',
+  darkGrey: '#323334',
+  lightGrey: '#A9A9A9',
+  grey: '#E7E7E7',
+};
