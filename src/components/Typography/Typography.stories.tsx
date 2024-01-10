4d00754299f3d56ddf1e2f34e98cae410497965c
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2024-01-10 18:38:18 -0500

    Create stories for Typography component

---
+++ src/components/Typography/Typography.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Typography } from './Typography';
+
+const meta: Meta<typeof Typography> = {
+  title: 'Atomic/Typography',
+  component: Typography,
+
+  tags: ['autodocs'],
+
+  argTypes: {
+    variant: {
+      options: ['h1', 'h2', 'h3', 'h4', 'subtitle1', 'subtitle2'],
+      control: { type: 'radio' },
+    },
+    children: {
+      control: { type: 'text' },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Typography>;
+
+export const Heading1: Story = {
+  args: {
+    variant: 'h1',
+    children: 'Heading 1',
+  },
+};
+
+export const Heading2: Story = {
+  args: {
+    variant: 'h2',
+    children: 'Heading 2',
+  },
+};
+
+export const Heading3: Story = {
+  args: {
+    variant: 'h3',
+    children: 'Heading 3',
+  },
+};
+
+export const Heading4: Story = {
+  args: {
+    variant: 'h4',
+    children: 'Heading 4',
+  },
+};
+
+export const Subtitle1: Story = {
+  args: {
+    variant: 'subtitle1',
+    children: 'Subtitle 1',
+  },
+};
+
+export const Subtitle2: Story = {
+  args: {
+    variant: 'subtitle2',
+    children: 'Subtitle 2',
+  },
+};
