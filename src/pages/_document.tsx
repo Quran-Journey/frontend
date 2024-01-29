cf3b174b31c30d37335fe9b28008cb4cdb4184ff
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2024-01-29 00:14:22 -0500

    Convert _document.tsx to typescript and remove emotion styling

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const initialProps = await Document.getInitialProps(ctx);
+  return { ...initialProps };
+};
+
+export default MyDocument;
