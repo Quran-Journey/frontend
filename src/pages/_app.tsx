d6a5eb4014fed5262923c83c49bde75061235095
SarahEddeb <sarah.eddeb@mail.utoronto.ca>
2024-01-28 22:18:29 -0500

    Remove unused styling functions and file imports from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import App, { AppProps } from 'next/app';
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { AppProps } from 'next/app';
 
-import createEmotionCache from '../utility/createEmotionCache';
-import theme from '../styles/theme/theme';
-import '../styles/globals.css';
-
-const clientSideEmotionCache = createEmotionCache();
-
-interface QJAppProps extends AppProps {
-  emotionCache: EmotionCache;
-}
-
-class QJApp extends App<QJAppProps> {
-  render() {
-    const emotionCache = clientSideEmotionCache;
-    const { Component, pageProps } = this.props;
-
-    return (
-      <CacheProvider value={emotionCache}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </CacheProvider>
-    );
-  }
-}
+const QJApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
 export default QJApp;
