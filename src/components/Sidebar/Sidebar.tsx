c2382093535111778ad98f49b2885751b0280ae1
SarahEddeb <sarah.ed123@gmail.com>
2024-02-03 12:35:17 -0500

    Add breakpoints to Sidebar component to adjust margin

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import { ChapterList, ChapterListProps } from '../ChapterList/ChapterList';
 import { LessonList, LessonListProps } from '../LessonList/LessonList';
 import { Button } from '../Button/Button';
 import { ButtonGroup } from '../ButtonGroup/ButtonGroup';
+import { breakpoints } from '@/styles/breakpoints';
 
 export interface SidebarProps {
   chapterData: ChapterListProps;
@@ -13,10 +14,38 @@ export interface SidebarProps {
 /**
  * Represents the styled component for the list div.
  */
-const SidebarDiv = styled.div`
+const SidebarContent = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+`;
+
+const Margin = styled.div`
+  margin: 0 10px;
+`;
+
+const SidebarDiv = styled.div`
+  margin: 22px 10px;
+
+  @media (${breakpoints.xl}) {
+    margin: 20px 10px;
+  }
+
+  @media (${breakpoints.lg}) {
+    margin: 20px 10px;
+  }
+
+  @media (${breakpoints.md}) {
+    margin: 20px 10px;
+  }
+
+  @media (${breakpoints.sm}) {
+    margin: 10px 8px;
+  }
+
+  @media (${breakpoints.xs}) {
+    margin: 8px 5px;
+  }
 `;
 
 export const Sidebar = ({
@@ -29,58 +58,63 @@ export const Sidebar = ({
   );
   return (
     <SidebarDiv>
-      {isLessonListDisabled ? (
-        <>
-          <ButtonGroup>
-            <Button
-              selected={isChapterSelected}
-              onClick={() => setChapterSelected(true)}
-            >
-              Chapter
-            </Button>
+      <SidebarContent>
+        {isLessonListDisabled ? (
+          <>
+            <Margin>
+              <ButtonGroup>
+                <Button
+                  selected={isChapterSelected}
+                  onClick={() => setChapterSelected(true)}
+                >
+                  Chapter
+                </Button>
 
-            <Button disabled={isLessonListDisabled}>
-              Lessons / Chapter...
-            </Button>
-          </ButtonGroup>
-
-          <ChapterList allChapters={chapterData.allChapters} />
-        </>
-      ) : (
-        <>
-          <ButtonGroup>
-            <Button
-              selected={isChapterSelected}
-              onClick={() => setChapterSelected(true)}
-            >
-              Chapter
-            </Button>
-            {lessonData ? (
-              <Button
-                selected={!isChapterSelected}
-                onClick={() => setChapterSelected(false)}
-              >
-                Lessons / {lessonData.headerDetails.surahName}{' '}
-              </Button>
-            ) : (
-              <Button disabled={true}>Lessons</Button>
-            )}
-          </ButtonGroup>
-
-          {isChapterSelected ? (
+                <Button disabled={isLessonListDisabled}>
+                  Lessons / Choose a Chapter
+                </Button>
+              </ButtonGroup>
+            </Margin>
             <ChapterList allChapters={chapterData.allChapters} />
-          ) : (
-            <>
-              {lessonData && (
-                <LessonList
-                  headerDetails={lessonData.headerDetails}
-                  allLessons={lessonData.allLessons}
-                />
-              )}
-            </>
-          )}
-        </>
-      )}
+          </>
+        ) : (
+          <>
+            <Margin>
+              <ButtonGroup>
+                <Button
+                  selected={isChapterSelected}
+                  onClick={() => setChapterSelected(true)}
+                >
+                  Chapter
+                </Button>
+                {lessonData ? (
+                  <Button
+                    selected={!isChapterSelected}
+                    onClick={() => setChapterSelected(false)}
+                  >
+                    Lessons / {lessonData.headerDetails.surahName}{' '}
+                  </Button>
+                ) : (
+                  <Button disabled={true}>Lessons</Button>
+                )}
+              </ButtonGroup>
+            </Margin>
+
+            {isChapterSelected ? (
+              <ChapterList allChapters={chapterData.allChapters} />
+            ) : (
+              <>
+                {lessonData && (
+                  <LessonList
+                    headerDetails={lessonData.headerDetails}
+                    allLessons={lessonData.allLessons}
+                  />
+                )}
+              </>
+            )}
+          </>
+        )}
+      </SidebarContent>
     </SidebarDiv>
   );
 };
