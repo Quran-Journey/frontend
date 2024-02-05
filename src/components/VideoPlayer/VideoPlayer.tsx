eaa47b76bc1746a0cb17a86c87dd369b7bcce269
SarahEddeb <sarah.ed123@gmail.com>
2024-02-05 15:04:48 -0500

    Add documentation to VideoPlayer component

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-interface VideoPlayerProps {
+/**
+ * Interface for the VideoPlayerProps.
+ * @property {string} src - The source of the video.
+ */
+export interface VideoPlayerProps {
   src: string;
 }
 
+/**
+ * Styled component for rendering an iframe that span the full width of its parent div.
+ */
 const VideoiFrame = styled.iframe`
   min-width: 100%;
   aspect-ratio: 16 / 9;
 `;
 
-export const VideoPlayer = ({ src }: VideoPlayerProps) => {
+/**
+ * Renders a video player component.
+ *
+ * @param {VideoPlayerProps} props - The props for the video player component.
+ * @param {string} props.src - The source URL of the video.
+ * @returns {JSX.Element} The rendered video player component.
+ */
+export const VideoPlayer = ({ src }: VideoPlayerProps): JSX.Element => {
   return <VideoiFrame src={src} />;
 };
