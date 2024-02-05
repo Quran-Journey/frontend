aed3de21a47cab517386dbfcd0a1af98a6b36c77
SarahEddeb <sarah.ed123@gmail.com>
2024-02-05 15:18:36 -0500

    Add additional iframe attributes to VideoPlayer component for fullscreen, title, and other video settings

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -4,9 +4,11 @@ import styled from 'styled-components';
 /**
  * Interface for the VideoPlayerProps.
  * @property {string} src - The source of the video.
+ * @property {string} [title] - The title of the video used for accessibilty purposes.
  */
 export interface VideoPlayerProps {
   src: string;
+  title?: string;
 }
 
 /**
@@ -22,8 +24,19 @@ const VideoiFrame = styled.iframe`
  *
  * @param {VideoPlayerProps} props - The props for the video player component.
  * @param {string} props.src - The source URL of the video.
+ * @param {string} props.title - The title of the video player.
  * @returns {JSX.Element} The rendered video player component.
  */
-export const VideoPlayer = ({ src }: VideoPlayerProps): JSX.Element => {
-  return <VideoiFrame src={src} />;
+export const VideoPlayer = ({
+  src,
+  title = 'Quran Journey - YouTube Video',
+}: VideoPlayerProps): JSX.Element => {
+  return (
+    <VideoiFrame
+      src={src}
+      title={title}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    />
+  );
 };
