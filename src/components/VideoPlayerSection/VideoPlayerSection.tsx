55c6537f35da2441c72daed4c0d4b2834507e35a
SarahEddeb <sarah.ed123@gmail.com>
2024-02-06 23:12:11 -0500

    Add breakpoints tp VideoPlayerSection to change layout on smaller screens

--- src/components/VideoPlayerSection/VideoPlayerSection.tsx
+++ src/components/VideoPlayerSection/VideoPlayerSection.tsx
@@ -4,6 +4,7 @@ import { Button } from '../Button/Button';
 import { ButtonGroup } from '../ButtonGroup/ButtonGroup';
 import { VideoHeader, VideoHeaderProps } from '../VideoHeader/VideoHeader';
 import { VideoPlayer, VideoPlayerProps } from '../VideoPlayer/VideoPlayer';
+import { breakpoints } from '../../styles/breakpoints';
 import {
   PDFDownloadButton,
   PDFDownloadButtonProps,
@@ -34,14 +35,42 @@ const HeaderSection = styled.div`
   flex-direction: row;
 
   margin: 38px 0;
+
+  @media (${breakpoints.sm}) {
+    gap: 15px;
+    flex-direction: column;
+    margin: 15px 0;
+
+  }
+
+  @media (${breakpoints.xs}) {
+    flex-direction: column;
+  }
 `;
 
 const HeaderItem = styled.div`
   width: 60%;
+
+  @media (${breakpoints.sm}) {
+    width: 100%;
+  }
 `;
 
 const HeaderDownload = styled.div`
   width: 36%;
+
+  @media (${breakpoints.sm}) {
+    width: 100%;
+  }
+`;
+
+const PDFButtonSection = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+  margin-bottom: 50px;
 `;
 
 export const VideoPlayerSection = ({
@@ -56,7 +85,7 @@ export const VideoPlayerSection = ({
         <Button startIcon={<ArrowLeftCircleFill />}>Previous</Button>
         <Button endIcon={<ArrowRightCircleFill />}>Next</Button>
       </ButtonGroup>
-      <VideoPlayer src={`${srcVideo.src}&rel=0`} title={srcVideo.title}/>
+      <VideoPlayer src={`${srcVideo.src}&rel=0`} title={srcVideo.title} />
       <HeaderSection>
         <HeaderItem>
           <VideoHeader
@@ -76,11 +105,13 @@ export const VideoPlayerSection = ({
           ></PDFDownloadButton>
         </HeaderDownload>
       </HeaderSection>
-      <PDFEmbed src={srcPDF.src}></PDFEmbed>
-      <ButtonGroup>
-        <Button startIcon={<ArrowLeftCircleFill />}>Previous</Button>
-        <Button endIcon={<ArrowRightCircleFill />}>Next</Button>
-      </ButtonGroup>
+      <PDFButtonSection>
+        <PDFEmbed src={srcPDF.src}></PDFEmbed>
+        <ButtonGroup>
+          <Button startIcon={<ArrowLeftCircleFill />}>Previous</Button>
+          <Button endIcon={<ArrowRightCircleFill />}>Next</Button>
+        </ButtonGroup>
+      </PDFButtonSection>
     </VideoSectionDiv>
   );
 };
