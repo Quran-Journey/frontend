ca4e2dc049c19eb62f7355034a4058d13b34ce7d
SarahEddeb <sarah.ed123@gmail.com>
2024-03-07 17:48:11 -0500

    Create basic expand/collapse Sidebar feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,72 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Typography } from '@/components/Typography/Typography';
 import { ResponsivePage } from '@/components/ResponsivePage/ResponsivePage';
+import { Sidebar, SidebarProps } from '@/components/Sidebar/Sidebar';
+import { Button } from '@/components/Button/Button';
+import {
+  sampleChapterData,
+  sampleHeaader,
+  sampleLessonData,
+  sampleSrcPDF,
+  sampleVideoHeader,
+  samplePdfDownloadInfo,
+  sampleVideoSrc,
+} from '@/mock_data/storybook_mock_data';
+import {
+  VideoPlayerSectionProps,
+  VideoPlayerSection,
+} from '@/components/VideoPlayerSection/VideoPlayerSection';
+
+const lessonData: SidebarProps = {
+  chapterData: { allChapters: sampleChapterData },
+  lessonData: {
+    headerDetails: sampleHeaader,
+    allLessons: sampleLessonData,
+  },
+};
+
+const videoSectionData: VideoPlayerSectionProps = {
+  headerData: sampleVideoHeader,
+  srcPDF: sampleSrcPDF,
+  pdfDownloadData: samplePdfDownloadInfo,
+  srcVideo: sampleVideoSrc,
+};
+
 /**
  * Renders the Home component.
  *
  * @returns {JSX.Element} The rendered Home component.
  */
 const Home: React.FC = (): JSX.Element => {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
   return (
     <ResponsivePage>
-      <Typography variant="h1">Welcome to Quran Journey</Typography>
+      <Button onClick={toggleSidebar}>
+        {isSidebarOpen ? 'Open Sidebar' : 'Close Sidebar'}
+      </Button>
+      {isSidebarOpen && (
+        <Sidebar
+          chapterData={lessonData.chapterData}
+          lessonData={lessonData.lessonData}
+        />
+      )}
+
+      {/* <Sidebar chapterData={lessonData.chapterData} lessonData={lessonData.lessonData}/> */}
+      <VideoPlayerSection
+        headerData={videoSectionData.headerData}
+        srcPDF={videoSectionData.srcPDF}
+        pdfDownloadData={videoSectionData.pdfDownloadData}
+        srcVideo={videoSectionData.srcVideo}
+      />
+      {/* <Typography variant="h1">Welcome to Quran Journey</Typography>
       <div>
         <Typography variant="subtitle1">This is a placeholder page</Typography>
-      </div>
+      </div> */}
     </ResponsivePage>
   );
 };
