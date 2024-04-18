e3dde0c412eddf3b8d0d053af11a420dc484d89b
SarahEddeb <sarah.ed123@gmail.com>
2024-04-18 14:01:12 -0400

    Add todo comments to HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { ResponsiveContainer } from '@/components/ResponsivePage/ResponsiveContainer';
 import { Sidebar, SidebarProps } from '@/components/Sidebar/Sidebar';
+import { NavBar } from '@/components/NavBar/NavBar';
+
+// TODO: Remove once fetch calls in place
 import {
   sampleChapterData,
   sampleHeaader,
   sampleLessonData,
 } from '@/mock_data/storybook_mock_data';
-import { NavBar } from '@/components/NavBar/NavBar';
 
+// TODO: Remove once fetch calls in place
 const lessonData1: SidebarProps = {
   chapterData: { allChapters: sampleChapterData },
   lessonData: {
@@ -25,6 +28,9 @@ export const HomePage: React.FC = ({
   chapterData = lessonData1.chapterData,
   lessonData = null,
 }: SidebarProps): JSX.Element => {
+
+  // TODO: fetch chapter data here
+
   return (
     <>
       <NavBar />
